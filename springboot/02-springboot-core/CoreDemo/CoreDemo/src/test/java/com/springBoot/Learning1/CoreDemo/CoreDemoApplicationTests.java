d6654f790e26ee47bf965fa2f145cf915e79af53
Kruthikshiv <Kruthik19102000@gmail.com>
2024-08-27 23:19:10 +0530

    Spring Core - Constructor Injection

---
+++ springboot/02-springboot-core/CoreDemo/CoreDemo/src/test/java/com/springBoot/Learning1/CoreDemo/CoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.Learning1.CoreDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
